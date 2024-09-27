12890cd6ef9d9f2f5dcfa58a3a7a512a6a5bbcf6
punjai77 <s6501075510389@email.kmutnb.ac.th>
2024-09-27 22:31:04 +0700

    Upload firsttime

---
+++ get.js
@@ -0,0 +1,15 @@
+function updateData() {
+  // แทนที่ด้วยโค้ดสำหรับดึงข้อมูลของคุณ
+  fetch("http://206.189.146.138/api/sensors")
+  .then(response => response.json())
+  .then(data => {
+    // document.getElementById('timestamp').textContent = data.timestamp;
+    document.getElementById('temperature').textContent = data.temperature.toFixed(2);
+    document.getElementById('luxsensor').textContent = data.luxsensor.toFixed(2);
+    document.getElementById('humidity').textContent = data.humidity.toFixed(2);
+    document.getElementById('motion').textContent = data.motion;
+    document.getElementById('timestamp').textContent = data.timestamp;
+  });
+}
+
+setInterval(updateData, 1000); // เรียกใช้ฟังก์ชัน updateData ทุก 1000 มิลลิวินาที (1 วินาที)
